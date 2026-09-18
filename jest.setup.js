d1f28f5e088ad35b32d5d05be1a41fc0ccc7d2b0
quelerir <lehandrk@yandex.ru>
2026-09-18 16:28:16 +0400

    fix: use proper AsyncStorage in-memory mock instead of stub

--- jest.setup.js
+++ jest.setup.js
@@ -1,15 +1,3 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-jest.mock('@react-native-async-storage/async-storage', () => ({
-  __esModule: true,
-  default: {
-    getItem: jest.fn(),
-    setItem: jest.fn(),
-    removeItem: jest.fn(),
-    multiGet: jest.fn(),
-    multiSet: jest.fn(),
-    multiRemove: jest.fn(),
-    getAllKeys: jest.fn(),
-    clear: jest.fn(),
-  },
-}));
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+);
